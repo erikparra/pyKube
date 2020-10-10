87c5056a5b485bb3ef9426f6ac69df94e88dbb98
Erik Parra <erik.parra4@gmail.com>
2020-10-10 10:24:37 -0500

    updated flask with 3 endpoints

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return "Welcome to the index page."
+
+@app.route('/hello/')
 def hello():
 	return "Hello World!"
+
+@app.route('/hello/<username>')
+def greet(username):
+	return 'Hi there, {}'.format(username)
 
 if __name__ == '__main__':
 	app.run()
